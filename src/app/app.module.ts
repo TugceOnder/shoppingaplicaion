ae8abdcfbc78707851975ad893e14f9f1fbb31d9
TugceOnder <tugceonder775@gmail.com>
2022-10-17 22:17:35 +0300

    router mimarisi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { AlertifyService } from './services/alertify.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { CategoryComponent } from './category/category.component';
@@ -13,13 +13,14 @@ import {HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
+    NavComponent,   
     CategoryComponent,
     ProductComponent,
     ProductFilterPipe
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
     HttpClientModule
   ],
